ccd4778a9e56595d541c0eafa3ff43977653c5e4
Valerie Gidzinski <valgidzi@gmail.com>
2018-12-18 14:22:22 -0800

    Install axios, add get request to Library component (not working)

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
-const Library = () => {
-  return (
-    <section>
-      <h2>This is Library.</h2>
-    </section>
-  )
+class Library extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('localhost:3000/movies')
+      .then((response) => {
+        console.log('Yay!');
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('Boo!');
+        console.log(error);
+      })
+  }
+
+  render() {
+    return (
+      <section>
+        <h2>This is Library.</h2>
+      </section>
+    )
+  }
+
 }
 
 export default Library;

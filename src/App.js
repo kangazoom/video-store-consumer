8cbf68fb620c816dd06337abd2ad277cad41127b
Valerie Gidzinski <valgidzi@gmail.com>
2018-12-19 10:50:04 -0800

    Move route paths to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import NavMenu from './components/NavMenu';
+import { Route } from 'react-router-dom';
+import MovieSearchForm from './components/MovieSearchForm';
+import Library from './components/Library';
+import Customers from './components/Customers';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +11,9 @@ class App extends Component {
     return (
       <div>
         <NavMenu />
+        <Route path="/search" component={MovieSearchForm} />
+        <Route path="/library" component={Library} />
+        <Route path="/customers" component={Customers} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import MovieSearchForm from './components/MovieSearchForm';
-import Library from './components/Library';
-import Customers from './components/Customers';
 import registerServiceWorker from './registerServiceWorker';
 
 const routing = (
   <Router>
     <div>
       <Route path="/" component={App} />
-      <Route path="/search" component={MovieSearchForm} />
-      <Route path="/library" component={Library} />
-      <Route path="/customers" component={Customers} />
     </div>
 </Router>
 

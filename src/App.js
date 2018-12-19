e88574ce9916296e50153a796be78b012866c0f4
kangazoom <michellelcronin@gmail.com>
2018-12-19 12:29:46 -0800

    set up clickback and event handling for movie-->library-->app

--- src/App.js
+++ src/App.js
@@ -7,12 +7,30 @@ import Customers from './components/Customers';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedMovie: 'None',
+      selectedCustomer: 'None'
+    };
+  }
+
+  selectMovieForRental = (movieTitle) => {
+    console.log(`In App: ${movieTitle}`);
+    const clickedMovie =
+    this.setState = ({
+      selectedMovie: movieTitle
+    })
+  }
+
   render() {
     return (
       <div>
         <NavMenu />
         <Route path="/search" component={MovieSearchForm} />
-        <Route path="/library" component={Library} />
+        <Route exact path="/library" render={(props) => (
+          <Library selectedMovieCB={this.selectMovieForRental} />
+        )}/>
         <Route path="/customers" component={Customers} />
       </div>
     );
--- src/components/Library.js
+++ src/components/Library.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import Movie from './Movie';
 
 class Library extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
@@ -13,27 +13,31 @@ class Library extends React.Component {
 
   componentDidMount() {
     axios.get('http://localhost:3000/movies')
-      .then((response) => {
-        this.setState({movies: response.data})
-      })
-      .catch((error) => {
-        this.setState({error: error.message})
-      })
+    .then((response) => {
+      this.setState({movies: response.data})
+    })
+    .catch((error) => {
+      this.setState({error: error.message})
+    })
   }
 
-  selectMovieForRental = (movieId) => {
+  findMovieForRental = (movieId) => {
     console.log(`In Library ${movieId}`);
+    const clickedMovie = this.state.movies.find( movie => movie.id === movieId)
+    this.props.selectedMovieCB(clickedMovie.title)
   }
 
   render() {
+    console.log(this.props)
+
     const movieCollection = this.state.movies.map((movie) => {
       return <Movie key={movie.id}
-      id={movie.id}
-      title={movie.title}
-      releaseDate={movie.release_date}
-      imageUrl={movie.image_url}
-      callback={this.selectMovieForRental}
-      buttonText='Select for Rental' />
+        id={movie.id}
+        title={movie.title}
+        releaseDate={movie.release_date}
+        imageUrl={movie.image_url}
+        callback={this.findMovieForRental}
+        buttonText='Select for Rental' />
     });
     return (
       <section>
